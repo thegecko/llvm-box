e9c398a6687e7688f583b3ac66485886fd848ad7
thegecko <rob.moran@arm.com>
2023-09-16 21:49:48 +0100

    no tty

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -11,7 +11,7 @@ popd
 mkdir -p $(pwd)/build/emsdk_cache
 
 docker run \
-    -it --rm \
+    --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(pwd):$(pwd) \
     -v $(pwd)/build/emsdk_cache:/emsdk/upstream/emscripten/cache \
