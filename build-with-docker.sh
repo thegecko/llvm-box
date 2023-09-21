9417159bd0691d72e2d26120e090d50b6044c1da
thegecko <rob.moran@arm.com>
2023-09-21 18:29:38 +0100

    reset

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 SRC=$(dirname $0)
+SRC=$(realpath "$SRC")
 
 pushd $SRC/docker
 docker build \
-    -t llvm_build \
+    -t emception_build \
     .
 popd
 
 mkdir -p $(pwd)/build/emsdk_cache
 
 docker run \
-    --rm \
+    -i --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(pwd):$(pwd) \
     -v $(pwd)/build/emsdk_cache:/emsdk/upstream/emscripten/cache \
     -u $(id -u):$(id -g) \
     $(id -G | tr ' ' '\n' | xargs -I{} echo --group-add {}) \
-    llvm_build:latest \
+    emception_build:latest \
     bash -c "cd $(pwd) && ./build.sh"
