f7665ee18a078ac56739a4b2acdeef3ee79d9cbb
thegecko <rob.moran@arm.com>
2023-10-18 01:33:45 +0200

    ref cXX

--- tooling/wasm-transform/compile.sh
+++ tooling/wasm-transform/compile.sh
@@ -13,4 +13,4 @@ SRC=$(realpath $SRC)
 BUILD=$(realpath $BUILD)
 WASM_UTILS=$(realpath $WASM_UTILS)
 
-clang++ -O3 -I$WASM_UTILS -std=c++20 $WASM_UTILS/*.cpp $SRC/wasm-transform.cpp -o $BUILD/wasm-transform
+${CXX} -O3 -I$WASM_UTILS -std=c++20 $WASM_UTILS/*.cpp $SRC/wasm-transform.cpp -o $BUILD/wasm-transform
