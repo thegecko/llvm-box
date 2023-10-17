efe096d75b2e48968ef264f3a40d7f55bd308bc8
thegecko <rob.moran@arm.com>
2023-10-18 01:42:35 +0200

    install git

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+SRC=$(dirname $0)
+
+pushd $SRC/docker
+docker build \
+    -t llvm_build \
+    .
+popd
+
 docker run \
     -i --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(pwd):$(pwd) \
     -u $(id -u):$(id -g) \
     $(id -G | tr ' ' '\n' | xargs -I{} echo --group-add {}) \
-    ghcr.io/webassembly/wasi-sdk:main \
+    llvm_build:latest \
     bash -c "cd $(pwd) && ./build.sh"
