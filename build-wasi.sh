58dc7bc18982b3280c563ab9b09b157228ed7fc7
thegecko <github@thegecko.org>
2023-10-26 14:09:44 +0200

    value

--- build-wasi.sh
+++ build-wasi.sh
@@ -40,7 +40,7 @@ LLVM_NATIVE=$BUILD/llvm-native
         -DLLVM_TABLEGEN=$LLVM_NATIVE/bin/llvm-tblgen \
         -DCLANG_TABLEGEN=$LLVM_NATIVE/bin/clang-tblgen \
         -DCMAKE_TOOLCHAIN_FILE=/usr/share/cmake/wasi-sdk-pthread.cmake \
-        -D_WASI_EMULATED_SIGNAL
+        -D_WASI_EMULATED_SIGNAL=True
 
     # Make sure we build js modules (.mjs).
     # The patch-ninja.sh script assumes that.
