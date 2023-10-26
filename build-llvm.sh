c57947c08cda8bba8368168e57211dca7f698c35
thegecko <github@thegecko.org>
2023-10-26 22:39:48 +0200

    use fork

--- build-llvm.sh
+++ build-llvm.sh
@@ -22,15 +22,16 @@ LLVM_NATIVE=$BUILD/llvm-native
 
 # If we don't have a copy of LLVM, make one
 if [ ! -d $LLVM_SRC/ ]; then
-    git clone --depth 1 https://github.com/llvm/llvm-project.git "$LLVM_SRC/"
+    git clone --depth 1 https://github.com/thegecko/llvm-project.git "$LLVM_SRC/"
 
     pushd $LLVM_SRC/
     
     # This is the last tested commit of llvm-project.
     # Feel free to try with a newer version
     #COMMIT=d5a963ab8b40fcf7a99acd834e5f10a1a30cc2e5
-    #git reset --hard $COMMIT
-    #git fetch origin $COMMIT
+    COMMIT=bfb079f2d811626009f1a67415a02b4255f52f51
+    git reset --hard $COMMIT
+    git fetch origin $COMMIT
 
     # The clang driver will sometimes spawn a new process to avoid memory leaks.
     # Since this complicates matters quite a lot for us, just disable that.
